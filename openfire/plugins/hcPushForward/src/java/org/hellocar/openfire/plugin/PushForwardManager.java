68a37d08d7a6a5ef9a05a1e7ccb9b5192bf7352d
yuuumy <yuuumy@gmail.com>
2013-03-13 02:21:43 +0800

    missing these two files

--- openfire/plugins/hcPushForward/src/java/org/hellocar/openfire/plugin/PushForwardManager.java
+++ openfire/plugins/hcPushForward/src/java/org/hellocar/openfire/plugin/PushForwardManager.java
@@ -1,5 +1,27 @@
 package org.hellocar.openfire.plugin;
 
-public class PushForwardManager {
+import java.lang.*;
 
+public class PushForwardManager implements IPushForwardManager, Runnable {
+	private Thread mainThread = null;
+	
+	public static IPushForwardManager CreateInstance() {
+		return new PushForwardManager();
+	}
+	
+	public PushForwardManager() {
+		mainThread = new Thread(this);
+	}
+	
+	public void Init() {
+		mainThread.start();
+	}
+	
+	public void Terminate() {
+		mainThread.join(millis);
+	}
+	
+	public void run() {
+        System.out.println("I'm running!");
+	}
 }
--- openfire/plugins/hcPushForward/src/java/org/hellocar/openfire/plugin/PushForwardPlugin.java
+++ openfire/plugins/hcPushForward/src/java/org/hellocar/openfire/plugin/PushForwardPlugin.java
@@ -1,64 +1,78 @@
 package org.hellocar.openfire.plugin;
 
 import java.io.File;
-
+import java.util.Map;
 import org.jivesoftware.openfire.*;
-import org.jivesoftware.openfire.container.Plugin;
-import org.jivesoftware.openfire.container.PluginManager;
-import org.jivesoftware.openfire.interceptor.InterceptorManager;
-import org.jivesoftware.openfire.interceptor.PacketInterceptor;
-import org.jivesoftware.openfire.interceptor.PacketRejectedException;
-import org.jivesoftware.openfire.plugin.rules.Rule;
-import org.jivesoftware.openfire.session.Session;
-import org.jivesoftware.util.JiveGlobals;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import org.jivesoftware.openfire.container.*;
+import org.jivesoftware.openfire.interceptor.*;
+import org.jivesoftware.openfire.event.*;
+import org.jivesoftware.openfire.session.*;
+import org.jivesoftware.openfire.user.User;
+import org.jivesoftware.util.*;
+import org.slf4j.*;
 import org.xmpp.packet.*;
 
-public class PushForwardPlugin implements Plugin, PacketInterceptor, OfflineMessageListener {
-	
-	private static final Logger tracer = LoggerFactory.getLogger(PushForwardPlugin.class);
-	private static PluginManager pluginManager;
-    private InterceptorManager interceptorManager;
-    private OfflineMessageStrategy offlineMssageStrategy;
-    private String pmName;
-    private PushForwardDBMapper dbMapper;
-    
+public class PushForwardPlugin implements Plugin, PacketInterceptor, OfflineMessageListener, UserEventListener {
+	private InterceptorManager interceptorManager = null;
+    private IPushForwardManager pushForwardManager = null;
+        
     public PushForwardPlugin() {
     	interceptorManager = InterceptorManager.getInstance();
-    	offlineMssageStrategy = XMPPServer.getInstance().getOfflineMessageStrategy();
-    	pmName = JiveGlobals.getProperty("plugin.hcPushForward.postman", "postman");
-    	dbMapper = new PushForwardDBMapper();
+    	pushForwardManager = PushForwardManager.CreateInstance();
+    	
+    	Utils.Debug("PushForwardPlugin created");
     }
 
     public void initializePlugin(PluginManager manager, File pluginDirectory) {
-    	tracer.info("HelloCar Push Forward Plugin loaded...");
-        pluginManager = manager;    
-        interceptorManager.addInterceptor(this);
-        offlineMssageStrategy.addListener(this);
+    	try {
+    		Configuration.Load();
+    		OfflineMessageStrategy.addListener(this);
+	        interceptorManager.addInterceptor(this);
+	        pushForwardManager.Init();
+	        
+	        Utils.Info("PushForwardPlugin loaded");
+    	}
+    	catch (Exception ex) {
+    		Utils.Error("Fail to load PushForwardPlugin", ex);
+    		throw ex;
+    	}
     }
 
     public void destroyPlugin() {
-    	tracer.info("HelloCar Push Forward Plugin destroyed ...");
-    	interceptorManager.removeInterceptor(this);
-    	offlineMssageStrategy.removeListener(this);
+    	try {
+    		OfflineMessageStrategy.removeListener(this);
+    		interceptorManager.removeInterceptor(this);
+    		pushForwardManager.Terminate();
+    		
+    		Utils.Info("PushForwardPlugin destroyed");
+    	}
+    	catch (Exception ex) {
+    		Utils.Error("Failed to destroy PushForwardPlugin", ex);
+    		throw ex;
+    	}
     }
     
     public void interceptPacket(Packet packet, Session session, boolean incoming, boolean processed) throws PacketRejectedException {
-    	try
-    	{
+    	try {
 	    	if (packet instanceof Message) {
 	            Message message = (Message)packet;
-	            String toNode = message.getTo().getNode();
-	            if (toNode.equalsIgnoreCase(this.pmName))
-	            {
-	            	dbMapper.AddPostmanMessage(message);
+	            if (message.getTo().getNode().equalsIgnoreCase(Configuration.PostmanName)) {
+	            	DBMapper.AddPostmanMessage(message);
+	            	Utils.Debug("New postman message arrived");
 	            }
 	        }
     	} catch (Exception ex) {
-    		tracer.error(ex.getMessage(), ex);
+    		Utils.Error("Fail to intercept postman message", ex);
+    	}
+    	return;
+    }
+    
+    public void messageStored(Message message) {
+    	try {
+    		DBMapper.AddOfflineMessage(message);
+    		Utils.Debug("New offline message arrived");
+    	} catch (Exception ex) {
+    		Utils.Error("Fail to process offline message", ex);
     	}
     	return;
     }
@@ -67,13 +81,21 @@ public class PushForwardPlugin implements Plugin, PacketInterceptor, OfflineMess
     	return;
     }
     
-    public void messageStored(Message message) {
-    	try
-    	{
-    		dbMapper.AddOfflineMessage(message);
-    	} catch (Exception ex) {
-    		tracer.error(ex.getMessage(), ex);
+    public void userCreated(User user, Map<String, Object> params) {
+    	try {
+    		int count = DBMapper.CheckMessageForUser(user);
+    		Utils.Debug(String.format("Checked message for user %1$s, count %2$d", user.getUsername(), count));
     	}
+    	catch (Exception ex) {
+    		Utils.Error(String.format("Failed to check message for user %1$s", user.getUsername()), ex);
+    	}
+    }
+
+    public void userDeleting(User user, Map<String, Object> params) {
     	return;
     }
-}       
+
+    public void userModified(User user, Map<String, Object> params) {
+    	return;
+    }
+}
